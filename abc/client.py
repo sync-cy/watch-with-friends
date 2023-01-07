af0c78a8a064fda3a5d7ab0b407c434e19027787
sreecharan3216 <sreecharan654321@gmail.com>
2023-01-07 17:34:44 +0530

    Co-authored-by: nazeem2001 <nazeem2001@users.noreply.github.com>

--- abc/client.py
+++ abc/client.py
@@ -10,6 +10,7 @@ port = 55555
 
 c= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c.connect((host, port))
+
 def re():
     while True:
         try:
@@ -32,13 +33,18 @@ wt=threading.Thread(target=write)
 # wt.start()
 def on_press(key):
     unmodified_key= Keyboard_listner.canonical(key)
-    c.send(f'{unmodified_key}_=P'.encode('ascii'))
-    c.send(f'{key}_=P'.encode('ascii'))
+    c.send(f'{find_Key_to_send(unmodified_key ,key)}_=P     '.encode('ascii'))
+   
     
+def find_Key_to_send(unmodified_key,key):
+    if len(str(unmodified_key))>len(str(key)): 
+        return unmodified_key
+    else:
+        return key
 
 def on_release(key):
     unmodified_key= Keyboard_listner.canonical(key)
-    c.send(f'{unmodified_key}_=R'.encode('ascii'))
+    c.send(f'{find_Key_to_send(unmodified_key ,key)}_=R     '.encode('ascii'))
 
 
 Keyboard_listner=Listener(on_press=on_press,on_release=on_release)
